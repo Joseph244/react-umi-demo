fae077ccfe7167f4a004b2a0539cdc8d8b2d9d7c
zhangzhenghfei <zhenghfeizhang@sina.com>
2022-06-10 17:33:30 +0800

    init2

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,49 @@
 import { history } from 'umi';
+import React, { useState } from 'react';
 const login = () => {
   debugger
   window.localStorage.setItem('user', "{name: 'admin'}")
   history.push('/user')
   return
 }
-export default function IndexPage() {
-  return (
-    <div>
-      <h2>Login</h2>
-      <form action="form_action.asp" method="get">
-        账号: <input type="text" name="account" />
-        密码: <input type="text" name="pwd" />
-        <input type="submit" value="提交" onClick={() => {
-            login()
-          }}/>
-      </form>
-    </div>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+    this.timerID;
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <form action="form_action.asp" method="get">
+          账号: <input type="text" name="account" />
+          密码: <input type="text" name="pwd" />
+          <input type="submit" value="提交" onClick={() => {
+              login()
+            }}/>
+        </form>
+      </div>
+    );
+  }
 }
+export default Login
